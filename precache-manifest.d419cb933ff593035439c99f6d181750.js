d35c14ffe6ef4806236cb7176c0074ee4a378abf
Gaurav Halvey <halveygaurav@gmail.com>
2020-05-29 02:43:22 +0530

    Updates

---
+++ precache-manifest.d419cb933ff593035439c99f6d181750.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35a81efb2a7be2b7694f5bcfba5841d1",
+    "url": "/ReactTodoListDemo/index.html"
+  },
+  {
+    "revision": "93b5273f4294c140fbe1",
+    "url": "/ReactTodoListDemo/static/css/main.8aad2bd0.chunk.css"
+  },
+  {
+    "revision": "1bfa80fb15bd975fc87a",
+    "url": "/ReactTodoListDemo/static/js/2.bafdceed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactTodoListDemo/static/js/2.bafdceed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93b5273f4294c140fbe1",
+    "url": "/ReactTodoListDemo/static/js/main.5a3be648.chunk.js"
+  },
+  {
+    "revision": "ff3c70fab9dd6d96186f",
+    "url": "/ReactTodoListDemo/static/js/runtime-main.03074c17.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactTodoListDemo/static/media/logo.5d5d9eef.svg"
+  }
+]);
